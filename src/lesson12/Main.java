0095ac152c09f35c394097b0bf4e7915c6a93b55
EliyevEmil <emilaliyev544@gmail.com>
2026-05-23 21:22:37 +0400

    lesson13

---
+++ src/lesson12/Main.java
@@ -0,0 +1,16 @@
+package lesson12;
+
+public class Main {
+    public static void main(String[] args) {
+        // Dəyişənlərin elan edilməsi
+        int a = 8;
+        int b = 3;
+        int c = 2;
+
+        // İfadənin hesablanması
+        int netice = a + b * c - a / c;
+
+        // Nəticənin ekrana çıxarılması
+        System.out.println("Hesablamanın nəticəsi: " + netice);
+    }
+}
