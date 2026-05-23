045862b8e9a6c04d4b4a43b7924b3605e0f74683
EliyevEmil <emilaliyev544@gmail.com>
2026-05-23 17:36:03 +0400

    lesson13

---
+++ src/lesson13/Main.java
@@ -0,0 +1,13 @@
+package lesson13;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 12;
+        System.out.println(a);
+        System.out.println("b deyisenin qiymeti:" + b);
+        String c = "13salam";
+        System.out.println(c);
+        System.out.println("Hello World");
+    }
+}
